92887fb300b8c629205268e6f0d9581d7ce8be38
ShinjiroMoriya <moriya@tam-tam.co.jp>
2017-09-11 11:06:05 +0900

    不要コード除去

--- line/cloudinary.py
+++ line/cloudinary.py
@@ -1,6 +1,5 @@
 from concurrent.futures import ThreadPoolExecutor
 from cloudinary.uploader import upload
-from line.logger import logger
 
 
 def image_upload(file, file_name):
@@ -12,6 +11,5 @@ def set_image_upload(file, file_name: str):
     try:
         return upload(file, public_id=file_name)
 
-    except Exception as ex:
-        logger.info(ex)
+    except:
         return None
